992da3d786b3bcd95e630ddad9a8d527d5ebef1c
exavince <exavince@live.be>
2021-06-03 13:56:37 +0200

    update

--- api/api.py
+++ api/api.py
@@ -11,6 +11,17 @@ app.config.from_object('config')
 mail = Mail(app)
 stop_mail = False
 
+def send_mail():
+    global stop_mail
+    if not stop_mail:
+        msg = Message('Web service HappyPhone issue !', sender = 'backup@support.mba.be', recipients = ['vince.grad@gmail.com'])
+        if sys.exc_info()[0]:
+            msg.body = "There is the error from the web service: \n\r" + str(sys.exc_info()[0])
+        else:
+            msg.body = "There is an error with the service worker \n\r"
+        mail.send(msg)
+        stop_mail = True
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -18,7 +29,7 @@ def home():
     
 @app.route('/api/callerid/<phone>', methods=['GET'])
 def get_phone(phone):
-    SQL = "EXEC [dbo].[HappyPhone_Global_Search_For_Phone_Display] @numPhone="
+    SQL = "EXEC [dbo].[HappyPhone_Global_Search_For_Phone_Display] @numPhone='"
 
     if phone == None:
         return "Error: No phone number", 400
@@ -29,20 +40,19 @@ def get_phone(phone):
     try:
         connection = pymssql.connect(server=app.config.get('SERVER'), user=app.config.get('USERNAME'), password=app.config.get('PASSWORD'), database=app.config.get('DATABASE'))  
         cursor = connection.cursor()
-        cursor.execute(SQL+phone)
+        cursor.execute(SQL+phone+"'")
         rows = cursor.fetchall()
+        rows = None
+        if rows is None:
+            send_mail()
+            return phone, 500
         return rows[0][0], 200
+        
     except:
         global stop_mail
-        if not stop_mail:
-            msg = Message('Web service HappyPhone issue !', sender = 'backup@support.mba.be', recipients = ['vince.grad@gmail.com'])
-            if sys.exc_info()[0]:
-                msg.body = "There is the error from the web service: \n\r" + str(sys.exc_info()[0])
-            else:
-                msg.body = "There is an error with the service worker \n\r"
-            mail.send(msg)
-            stop_mail = True
-
+        print(sys.exc_info()[0])
+        send_mail()
+        
         return phone, 500
 
 app.run()
